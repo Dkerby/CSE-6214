dcf6f83791d8a90bee3aed8aa5d785e92df7acbf
Drew Kerby <alk203@msstate.edu>
2018-03-06 20:28:40 -0600

    Add import and export funcation to NumberList

--- src/NumberList.py
+++ src/NumberList.py
@@ -8,3 +8,22 @@ class NumberList():
     def generateRandom(self, size):
         self.numbers = random.sample(xrange(1, 10000), size)
         self.length = len(self.numbers)
+
+    def importListFromFile(self, filename):
+        f = open(filename, "r")
+        tempList = []
+
+        line = f.readline()
+        line = line.replace(" ", "")
+        tempList = line.split(',')
+        self.numbers = tempList
+
+        f.close()
+
+    def exportListToFile(self, filename):
+        file = open(filename,"w")
+
+        for num in self.numbers:
+            file.write(str(num) + ',')
+
+        file.close()
