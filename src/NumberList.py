03d8b14109c621ef24c709a88b4725484310f443
Drew Kerby <alk203@msstate.edu>
2018-03-06 20:11:13 -0600

    Updated NumberList

--- src/NumberList.py
+++ src/NumberList.py
@@ -3,6 +3,8 @@ import random
 class NumberList():
     def __init__(self, numbers = []):
         self.numbers = numbers
+        self.length = len(numbers)
 
     def generateRandom(self, size):
         self.numbers = random.sample(xrange(1, 10000), size)
+        self.length = len(self.numbers)
