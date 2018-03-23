24e03fa57857fa34d926ec8252dc48ae4eceffb7
Jake Frulla <jake.frulla@gmail.com>
2018-03-23 16:57:24 -0500

    updated State.py

--- src/State.py
+++ src/State.py
@@ -1,26 +1,26 @@
 class State():
     def __init__(self, NumberObj,sorting = False,speed=0.05):
 
-	self.data=NumberObj
+        self.data=NumberObj
         self.sorting = sorting
-	self.speed=float(speed)
+        self.speed=float(speed)
 
-	#the rest of the data members start at zero.
-	#adjust manually to change them.
-	self.currentLine = 0
+        #the rest of the data members start at zero.
+        #adjust manually to change them.
+        self.currentLine = 0
         self.compares = 0
         self.swaps = 0
         self.memUsage = float(0.0)
         self.runtime = float(0.0)
-	self.i=0
-	self.j=0
-    
-    # def dump(self):
-    #     print("x:", self.x)
-    #     print("y:", self.y)
-    #     print("y:", self.z)
+        self.i=0
+        self.j=0
+        
+        # def dump(self):
+        #     print("x:", self.x)
+        #     print("y:", self.y)
+        #     print("y:", self.z)
 
-    # def set_values(self,A,B,C):
-    #     self.x = A
-    #     self.y= B
-    #     self.z= C
+        # def set_values(self,A,B,C):
+        #     self.x = A
+        #     self.y= B
+        #     self.z= C
