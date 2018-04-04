ab5ab536fac6281ae9a3b45e964f5957bd9e7990
Drew Kerby <alk203@msstate.edu>
2018-04-03 20:14:28 -0500

    Added the ability to read in using a file

---
+++ src/sortCLI.py
@@ -0,0 +1,185 @@
+from __future__ import print_function
+
+import os,sys, getopt, platform
+#os used for clearing the cmdline output, 
+#sys and geopt used for cmdline arguments
+
+import Algorithm as alg # holds algorithm
+
+################################################################################
+# Used for testing and formatting of command line output                       #
+################################################################################
+
+
+#MAIN PROGRAM
+def main(argv):
+
+        #default values for the sort
+        arrsize=200
+        algochoice=1
+        MAXARRAYSIZE=2048
+        wait=False
+        benchmark=False
+        speed=0.05
+
+        
+        #parsing of command line options
+        try:
+                opts, args = getopt.getopt(argv,"bhwf:r:a:s:",["filename=", "randnum=", "algo=", "spd="])
+
+        #error prints out the help info
+        except getopt.GetoptError:
+                print('\n\ttest.py\n\t[-r <size of array>]\n\t[-a <chosen algorithm (1-4)>]\n\t[-s <speed of algorithm>]')
+                print('\n\t-h displays help info')
+                print('\t-w toggles on the WAIT_FOR_USER step option\n')
+                print('\t1 - Insertion Sort')
+                print('\t2 - Bubble Sort')
+                print('\t3 - Quick Sort')
+                print('\t4 - Merge Sort')
+
+                sys.exit(2)
+
+        #for each option in the command line arguments
+        for opt, arg in opts:
+
+                # -h prints out help info
+                if opt == '-h':
+                        print('\n\ttest.py\n\t[-r <size of array>]\n\t[-a <chosen algorithm (1-4)>]\n\t[-s <speed of algorithm>]')
+                        print('\n\t-h displays help info')
+                        print('\t-w toggles on the WAIT_FOR_USER step option\n')
+                        print('\t1 - Insertion Sort')
+                        print('\t2 - Bubble Sort')
+                        print('\t3 - Quick Sort')
+                        print('\t4 - Merge Sort')
+                        sys.exit()
+
+                # -r lets user set the size of the randomly generated array
+                elif opt in ("-r", "--randnum"):
+                        arrsize = int(arg)
+
+                elif opt=="-f":
+                        arrsize = 0
+                        numList = []
+                        f = open(arg, "r")
+                        line = f.readline()
+                        for u in line.split():
+                            numList.append(u)
+                            arrsize = arrsize + 1
+
+                        #creates NumberList, State, and Algorithm objects
+                        x=alg.Algorithm(True, algochoice, speed, arrsize) #pass State object into Algorithm along with the choice of algorithm
+                        x.setData(numList)
+                        state=x.getState()
+
+                # -a lets the user choose which algorithm they want to do
+                elif opt in ("-a", "--algo"):
+                        algochoice = int(arg)
+
+                # -s lets the user set the speed of the stepping
+                elif opt in ("-s", "--spd"):
+                        speed=float(arg)
+
+                # -w is a toggle on the <press enter to step> portion of the output
+                elif opt =="-w":
+                        wait=True
+
+                elif opt=="-b":
+                        benchmark=True
+                
+                    
+        
+        #creates NumberList, State, and Algorithm objects
+        x=alg.Algorithm(True, algochoice, speed, arrsize) #pass State object into Algorithm along with the choice of algorithm
+        state=x.getState()
+        #print error and exit if the user's options are silly
+        if(arrsize<1 or arrsize>MAXARRAYSIZE):
+                print("Array inappropriately sized.")
+                sys.exit(2)
+        if(algochoice<1 or algochoice>5):
+                print("That is not one of the available algorithms")
+                sys.exit(2)
+
+        if(speed<0.0001 or speed>5.0):
+                print("That is either too fast or too slow")
+                sys.exit(2)
+
+        print('Generating array of size', arrsize)
+        print('Algorithm choice is ', algochoice)
+
+        #while algorithm is not complete, keep on steppin'
+        if(benchmark):
+                x.benchsetup(arrsize)
+                while(state.benchmarking):
+                        x.benchstep()
+                        state=x.getState()
+                        output=x.getFormat()
+                        
+                        #ADD I, J, SWAP, AND COMPARISON DATA TO OUTPUT
+                        output+="\n\ni:\t\t"+str(state.i)
+                        output+="\nj:\t\t"+str(state.j)
+                        output+="\nComparisons:\t"+str(state.compares)
+                        output+="\nMovements:\t"+str(state.swaps)
+                        output+="\nMemory Used:\t"+str(state.memUsage)+" Bytes"
+                        output+="\nRuntime: \t"+str("{0:.2f}".format(state.runtime))+" Seconds\n\n"
+
+
+                        #Higlighting the pseudocode
+                        for line in x.algo.lines:
+                                if (x.algo.lines[state.currentLine]==line):
+                                        output+= '\x1b[0;37;41m' + line + '\x1b[0m'
+                                else:
+                                        output+=line
+                        
+                        #clear stdout
+                        os.system('clear')
+
+                        #print to cmdline
+                        print(output)
+
+
+                        #this is where the -w option comes into play
+                        if(wait):
+                                input("<hit enter to step>")
+        else:
+                while(state.sorting):
+
+                        #step it up!
+                        x.step()
+                        state=x.getState()
+                        output=x.getFormat()
+
+                        #ADD I, J, SWAP, AND COMPARISON DATA TO OUTPUT
+                        output+="\n\ni:\t\t"+str(state.i)
+                        output+="\nj:\t\t"+str(state.j)
+                        output+="\nComparisons:\t"+str(state.compares)
+                        output+="\nMovements:\t"+str(state.swaps)
+                        output+="\nMemory Used:\t"+str(state.memUsage)+" Bytes"
+                        output+="\nRuntime: \t"+str("{0:.2f}".format(state.runtime))+" Seconds\n\n"
+
+
+                        #Higlighting the pseudocode
+                        for line in x.algo.lines:
+                                if (x.algo.lines[state.currentLine]==line):
+                                        output+= '\x1b[0;37;41m' + line + '\x1b[0m'
+                                else:
+                                        output+=line
+                        
+                        #clear stdout
+                        if(platform.system() == "Windows"):
+                                os.system("CLS")
+                        else:
+                                os.system('clear')
+
+                        #print to cmdline
+                        print(output)
+
+
+                        #this is where the -w option comes into play
+                        if(wait):
+                                input("<hit enter to step>")
+
+
+
+#random control flow to help parse cmdline args
+if __name__ == "__main__":
+        main(sys.argv[1:])
